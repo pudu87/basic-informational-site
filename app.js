5b9c3284f2c551d5f3f8d880ccbfdfdc20dab7d1
pudu87 <sam.cappelle87@gmail.com>
2021-11-19 18:50:06 -0300

    Add css

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 const http = require('http');
+const path = require('path')
 const fs = require('fs');
-const port = 8080;
+const PORT = 8080;
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  
-  let path = './views/';
-  switch(req.url) {
-    case '/':
-      path += 'index.html';
-      res.statusCode = 200;
+const server = http.createServer((req, res) => {  
+  let pathName;
+  if (path.extname(req.url) === '.css') {
+    pathName = './public/styles.css';
+    res.setHeader('Content-Type', 'text/css');
+  } else {
+    pathName = './views/';
+    res.setHeader('Content-Type', 'text/html')
+    switch(req.url) {
+      case '/':
+        pathName += 'index.html';
+        res.statusCode = 200;
+        break;
+      case '/about':
+        pathName += 'about.html';
+        res.statusCode = 200;
+        break;
+      case '/contact-me':
+        pathName += 'contact-me.html';
+        res.statusCode = 200;
+        break;
+      default:
+        pathName += '404.html';
+        res.statusCode = 404;
       break;
-    case '/about':
-      path += 'about.html';
-      res.statusCode = 200;
-      break;
-    case '/contact-me':
-      path += '/contact-me.html';
-      res.statusCode = 200;
-      break;
-    default:
-      path += '/404.html';
-      res.statusCode = 404;
-    break;
+    }
   }
 
-  fs.readFile(path, (err, data) => {
+  fs.readFile(pathName, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
@@ -35,6 +41,6 @@ const server = http.createServer((req, res) => {
   })
 });
 
-server.listen(port, 'localhost', () => {
-  console.log(`server running at ${port}`)
+server.listen(PORT, 'localhost', () => {
+  console.log(`server running at ${PORT}`)
 })
